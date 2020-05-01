3293ca596701d5b341c0e2afca3a9055510729e5
Oreo097 <ztdeyouxiang@qq.com>
2020-05-01 11:15:40 +0800

    new file:   "AD\345\272\223/History/KeyBoard.~(1).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(1).SchLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(10).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(11).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(12).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(2).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(2).SchLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(3).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(3).SchLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(4).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(5).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(6).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(7).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(8).PcbLib.Zip" 	new file:   "AD\345\272\223/History/KeyBoard.~(9).PcbLib.Zip" 	new file:   "AD\345\272\223/History/keyboradLib.~(1).LibPkg.Zip" 	new file:   "AD\345\272\223/KeyBoard.PcbLib" 	new file:   "AD\345\272\223/KeyBoard.SchLib" 	modified:   "AD\345\272\223/keyboradLib.LibPkg" 	modified:   "\345\211\215\346\234\237\345\256\236\351\252\214/Arduino\347\256\227\346\263\225\345\256\236\351\252\214/keyboard/keyboard.ino" 	new file:   "\350\207\252\345\267\261\350\256\276\350\256\241\347\232\204\351\224\256\347\233\230/History/Test.~(1).SchDoc.Zip" 	new file:   "\350\207\252\345\267\261\350\256\276\350\256\241\347\232\204\351\224\256\347\233\230/PCB/History/Test.~(1).PcbDoc.Zip" 	new file:   "\350\207\252\345\267\261\350\256\276\350\256\241\347\232\204\351\224\256\347\233\230/PCB/Test.PcbDoc" 	new file:   "\350\207\252\345\267\261\350\256\276\350\256\241\347\232\204\351\224\256\347\233\230/PCB/keyboard.PrjPCB" 	new file:   "\350\207\252\345\267\261\350\256\276\350\256\241\347\232\204\351\224\256\347\233\230/Test.SchDoc" 	new file:   "\350\207\252\345\267\261\350\256\276\350\256\241\347\232\204\351\224\256\347\233\230/__Previews/Test.SchDocPreview"

--- 前期实验/Arduino算法实验/keyboard/keyboard.ino
+++ 前期实验/Arduino算法实验/keyboard/keyboard.ino
@@ -1,15 +1,16 @@
-
+/*这是前期实验Arduino算法，主要用于扫描算法的检查*/
 
 #define LED 2
 
 #define Row1 3
 #define Row2 4
-#define Row3 5
 
-#define Col1 6
-#define Col2 7
-#define Col3 8
-#define Col4 9
+#define Col1 5
+#define Col2 6
+#define Col3 7
+#define Col4 8
+#define Col5 9
+#define Col6 10
 
 void setup()
 {
@@ -18,12 +19,12 @@ void setup()
   digitalWrite(LED, HIGH);
   pinMode(Row1, INPUT);
   pinMode(Row2, INPUT);
-  pinMode(Row3, INPUT);
 
   pinMode(Col1, OUTPUT);
   pinMode(Col2, OUTPUT);
   pinMode(Col3, OUTPUT);
   pinMode(Col4, OUTPUT);
+  pinMode(Col5, OUTPUT);
   delay(500);
   digitalWrite(LED, LOW);
   Serial.begin(9600);
@@ -34,7 +35,7 @@ void loop()
 {
   struct matrix
   {
-    int key[12]={0};
+    int key[12] = {0};
     int *inter = key;
   } matrix;
   digitalWrite(LED, HIGH);
@@ -42,9 +43,8 @@ void loop()
   digitalWrite(Col2, LOW);
   digitalWrite(Col3, LOW);
   digitalWrite(Col4, LOW);
-  // digitalWrite(Row1, LOW);
-  // digitalWrite(Row2, LOW);
-  // digitalWrite(Row3, LOW);
+  digitalWrite(Col5, LOW);
+  digitalWrite(Col6, LOW);
   delay(1); //延迟1ms使电平平稳
   if (digitalRead(Row1) == HIGH)
   {
@@ -56,18 +56,13 @@ void loop()
     *matrix.inter = 1;
     matrix.inter++;
   }
-  if (digitalRead(Row3) == HIGH)
-  {
-    *matrix.inter = 1;
-    matrix.inter++;
-  }
+
   digitalWrite(Col1, LOW);
   digitalWrite(Col2, HIGH);
   digitalWrite(Col3, LOW);
   digitalWrite(Col4, LOW);
-  // digitalWrite(Row1, LOW);
-  // digitalWrite(Row2, LOW);
-  // digitalWrite(Row3, LOW);
+  digitalWrite(Col5, LOW);
+  digitalWrite(Col6, LOW);
   delay(1); //延迟1ms使电平平稳
   if (digitalRead(Row1) == HIGH)
   {
@@ -75,11 +70,6 @@ void loop()
     matrix.inter++;
   }
   if (digitalRead(Row2) == HIGH)
-  {
-    *matrix.inter = 1;
-    matrix.inter++;
-  }
-  if (digitalRead(Row3) == HIGH)
   {
     *matrix.inter = 1;
     matrix.inter++;
@@ -88,9 +78,8 @@ void loop()
   digitalWrite(Col2, LOW);
   digitalWrite(Col3, HIGH);
   digitalWrite(Col4, LOW);
-  // digitalWrite(Row1, LOW);
-  // digitalWrite(Row2, LOW);
-  // digitalWrite(Row3, LOW);
+  digitalWrite(Col5, LOW);
+  digitalWrite(Col6, LOW);
   delay(1); //延迟1ms使电平平稳
   if (digitalRead(Row1) == HIGH)
   {
@@ -102,7 +91,53 @@ void loop()
     *matrix.inter = 1;
     matrix.inter++;
   }
-  if (digitalRead(Row3) == HIGH)
+  digitalWrite(Col1, LOW);
+  digitalWrite(Col2, LOW);
+  digitalWrite(Col3, LOW);
+  digitalWrite(Col4, HIGH);
+  digitalWrite(Col5, LOW);
+  digitalWrite(Col6, LOW);
+  delay(1); //延迟1ms使电平平稳
+  if (digitalRead(Row1) == HIGH)
+  {
+    *matrix.inter = 1;
+    matrix.inter++;
+  }
+  if (digitalRead(Row2) == HIGH)
+  {
+    *matrix.inter = 1;
+    matrix.inter++;
+  }
+  digitalWrite(Col1, LOW);
+  digitalWrite(Col2, LOW);
+  digitalWrite(Col3, LOW);
+  digitalWrite(Col4, LOW);
+  digitalWrite(Col5, HIGH);
+  digitalWrite(Col6, LOW);
+  delay(1); //延迟1ms使电平平稳
+  if (digitalRead(Row1) == HIGH)
+  {
+    *matrix.inter = 1;
+    matrix.inter++;
+  }
+  if (digitalRead(Row2) == HIGH)
+  {
+    *matrix.inter = 1;
+    matrix.inter++;
+  }
+  digitalWrite(Col1, LOW);
+  digitalWrite(Col2, LOW);
+  digitalWrite(Col3, LOW);
+  digitalWrite(Col4, LOW);
+  digitalWrite(Col5, LOW);
+  digitalWrite(Col6, HIGH);
+  delay(1); //延迟1ms使电平平稳
+  if (digitalRead(Row1) == HIGH)
+  {
+    *matrix.inter = 1;
+    matrix.inter++;
+  }
+  if (digitalRead(Row2) == HIGH)
   {
     *matrix.inter = 1;
     matrix.inter++;
@@ -112,4 +147,5 @@ void loop()
     Serial.println(matrix.key[i]);
     delay(2);
   }
+  delay(500);
 }
